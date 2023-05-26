36a030e7e3c58a1b2d098d21aaaaee10331fda32
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-05-26 16:17:39 +0100

    added a function that uses a regular expression with the g modifier to replace multiple substrings within a string

--- scripts/script.js
+++ scripts/script.js
@@ -12,20 +12,26 @@ function storeTextArea() {
   }
 }
 
+function boldString(word, subword) {
+  var strRegExp = new RegExp(subword, "g");
+  return word.replace(strRegExp, "<span>" + subword + "</span>");
+}
+
 function convertTextArea() {
   let convertedWordsArray = [];
   textAreaContent.split(" ").forEach((word) => {
     if (word) {
-      let splitWord = word.split("");
-      console.log(splitWord);
-      for (let index = 0; index < splitWord.length / 2; index++) {
-        let boldLetters = [splitWord[index]];
-        console.log(`<span>${boldLetters}</span>`);
-      }
-
-      // word = `<span> ${word} </span>`;
       convertedWordsArray += word + " ";
       convertedText.innerHTML = convertedWordsArray;
+      //Splits the words then logs them to the console
+      // let splitWord = word.split("");
+      // console.log(splitWord);
+      // for (let index = 0; index < splitWord.length / 2; index++) {
+      //   let boldLetters = [splitWord[index]];
+      //   console.log(`<span>${boldLetters}</span>`);
+      // }
+
+      // word = `<span> ${word} </span>`;
     }
   });
 }
