e6780e19bcd488a0807c3aa26d4bdfb9cec276a0
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-06-12 18:18:56 +0100

    removed the commented out old code. Need to work on the maths of slice as we're missing a letter at the end of the first half of the word

--- scripts/script.js
+++ scripts/script.js
@@ -30,15 +30,6 @@ function convertTextArea() {
       let spanAddedToFirstHalf = `<span>${firstHalfOfWord}</span>`;
       console.log(spanAddedToFirstHalf);
 
-      // for loop over result of split that adds an opening and closing span tag to the first half of the word
-      // for (let index = 0; index < splitWord.length / 2; index++) {
-      //   boldLetters += [splitWord[index]];
-      // }
-      // let spanAddedToBoldLetters = `<span>${boldLetters}</span>`;
-      // console.log(spanAddedToBoldLetters);
-
-      // slices the split word and returns the second half of the word
-
       let secondHalfOfWord = splitWord
         .slice(Math.ceil(splitWord.length / 2))
         .join("")
