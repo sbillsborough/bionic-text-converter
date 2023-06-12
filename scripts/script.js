0cdfad9d51ae13566f2c48c488753678a6583e59
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-06-12 18:16:43 +0100

    first half of the word is now sliced from 0 up to half of the length of the word and span class is added before and after the resulting string instead of after each individual letter

--- scripts/script.js
+++ scripts/script.js
@@ -22,14 +22,23 @@ function convertTextArea() {
       // Splits the words
       let splitWord = word.split("");
 
+      let firstHalfOfWord = splitWord
+        .slice(0, splitWord.length / 2)
+        .join("")
+        .trim();
+
+      let spanAddedToFirstHalf = `<span>${firstHalfOfWord}</span>`;
+      console.log(spanAddedToFirstHalf);
+
       // for loop over result of split that adds an opening and closing span tag to the first half of the word
-      for (let index = 0; index < splitWord.length / 2; index++) {
-        boldLetters += [splitWord[index]];
-      }
-      let spanAddedToBoldLetters = `<span>${boldLetters}</span>`;
-      console.log(spanAddedToBoldLetters);
+      // for (let index = 0; index < splitWord.length / 2; index++) {
+      //   boldLetters += [splitWord[index]];
+      // }
+      // let spanAddedToBoldLetters = `<span>${boldLetters}</span>`;
+      // console.log(spanAddedToBoldLetters);
 
       // slices the split word and returns the second half of the word
+
       let secondHalfOfWord = splitWord
         .slice(Math.ceil(splitWord.length / 2))
         .join("")
