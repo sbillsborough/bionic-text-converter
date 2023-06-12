696969d85e8613dc88d58847109f3b7abe4c9b3d
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-06-12 18:03:07 +0100

    span is now added to the first half of the word but it's adding all of the first half of the wrds together too

--- scripts/script.js
+++ scripts/script.js
@@ -14,19 +14,21 @@ function storeTextArea() {
 
 function convertTextArea() {
   let convertedWordsArray = [];
+  var boldLetters = [];
   textAreaContent.split(" ").forEach((word) => {
     if (word) {
       convertedWordsArray += word + " ";
       convertedText.innerHTML = convertedWordsArray;
       // Splits the words
       let splitWord = word.split("");
+
       // for loop over result of split that adds an opening and closing span tag to the first half of the word
       for (let index = 0; index < splitWord.length / 2; index++) {
-        let boldLetters = [splitWord[index]];
-
-        let spanAddedToBoldLetters = `<span>${boldLetters}</span>`;
-        console.log(spanAddedToBoldLetters);
+        boldLetters += [splitWord[index]];
       }
+      let spanAddedToBoldLetters = `<span>${boldLetters}</span>`;
+      console.log(spanAddedToBoldLetters);
+
       // slices the split word and returns the second half of the word
       let secondHalfOfWord = splitWord
         .slice(Math.ceil(splitWord.length / 2))
