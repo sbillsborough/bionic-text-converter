736e313c6221d49e39ea70f7521f2207d067f76c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-06-12 17:38:41 +0100

    cleaned up the comments

--- scripts/script.js
+++ scripts/script.js
@@ -18,17 +18,21 @@ function convertTextArea() {
     if (word) {
       convertedWordsArray += word + " ";
       convertedText.innerHTML = convertedWordsArray;
-      // Splits the words then logs them to the console
+      // Splits the words
       let splitWord = word.split("");
+      // for loop over result of split that adds an opening and closing span tag to the first half of the word
       for (let index = 0; index < splitWord.length / 2; index++) {
         let boldLetters = [splitWord[index]];
-        console.log(`<span>${boldLetters}</span>`);
+
+        let spanAddedToBoldLetters = `<span>${boldLetters}</span>`;
+        console.log(spanAddedToBoldLetters);
       }
+      // slices the split word and returns the second half of the word
       let secondHalfOfWord = splitWord
         .slice(Math.ceil(splitWord.length / 2))
         .join("")
         .trim();
-      // console.log(splitWord);
+
       console.log(secondHalfOfWord);
     }
   });
